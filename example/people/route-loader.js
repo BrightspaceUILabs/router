60000e7fa78b3e941732806ff2691e2ace982de7
Dave Lockhart <dlockhart@users.noreply.github.com>
2022-01-18 13:41:27 -0500

    fix: non-breaking Lit 2 preparation fixes

--- example/people/route-loader.js
+++ example/people/route-loader.js
@@ -1,5 +1,5 @@
 import { html } from 'lit-element';
-import { ifDefined } from 'lit-html/directives/if-defined';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
 import { redirect } from '../../router.js';
 
 export function loader() {
